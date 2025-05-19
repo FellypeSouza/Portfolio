b6c5ded6cffcf9fb883c5e714737e8bca20246c9
FellypeSouza <fellypedev23@gmail.com>
2025-05-19 12:53:58 -0300

    criando e estilizando seção 'sobre mim'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from '../components/nav/nav.component';
 import { HeaderComponent } from '../components/header/header.component';
+import { AboutMeComponent } from '../components/about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, HeaderComponent],
+  imports: [RouterOutlet, NavComponent, HeaderComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
