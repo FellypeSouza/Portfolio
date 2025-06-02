740a403b394709151ff03af60787166b0aa51c86
Fellype Souza <fellypedev23@gmail.com>
2025-06-02 20:26:36 -0300

    estilizando as seções de experiência e projetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from '../components/nav/nav.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { AboutMeComponent } from '../components/about-me/about-me.component';
+import { ExperienceComponent } from '../components/experience/experience.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, HeaderComponent, AboutMeComponent],
+  imports: [RouterOutlet, NavComponent, HeaderComponent, AboutMeComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
