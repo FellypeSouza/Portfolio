fa88d102be2d54fa7e2a010c756444e6237ef3f6
Fellype Souza <fellypedev23@gmail.com>
2025-06-02 23:16:10 -0300

    criando seção de projetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { NavComponent } from '../components/nav/nav.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { AboutMeComponent } from '../components/about-me/about-me.component';
 import { ExperienceComponent } from '../components/experience/experience.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { ContactsComponent } from '../components/contacts/contacts.component';
 import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, HeaderComponent, AboutMeComponent, ExperienceComponent],
+  imports: [RouterOutlet, NavComponent, HeaderComponent, AboutMeComponent, ExperienceComponent, ProjectsComponent, ContactsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
