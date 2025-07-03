140cabaeedb1739dd2df2e7fbf4ec22e3cc483a1
Fellype Souza <fellypedev23@gmail.com>
2025-07-03 19:24:54 -0300

    adicionando responsividade ao projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, HeaderComponent, AboutMeComponent, ExperienceComponent, ProjectsComponent, ContactsComponent, FooterComponent],
+  imports: [NavComponent, HeaderComponent, AboutMeComponent, ExperienceComponent, ProjectsComponent, ContactsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
